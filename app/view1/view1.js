f86b9891581ae3d64e0f5d32d6588af71c712f01
Mantas Sutula <msutula@scottlogic.co.uk>
2015-08-13 17:00:33 +0100

    Frontend display/update/create TODO

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,7 +1,8 @@
 'use strict';
 
-angular.module('todoList.view1', ['ngRoute'])
-
+angular.module('todoList.view1', [
+  'ngRoute'
+])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
@@ -9,6 +10,82 @@ angular.module('todoList.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', [function(TodoModel) {
+    var main = this;
 
+      main.loading = false;
+
+      main.newTodo = {
+        title: '',
+        isComplete: false
+      };
+
+      main.resetForm = function() {
+        main.loading = false;
+        main.newTodo = {
+          title: '',
+          isComplete: false
+        }
+      };
+
+      main.getTodos = function() {
+        main.todos = {
+          1: {
+            title: 'Scare Mantas',
+            isComplete: true
+          },
+          2: {
+            title: 'Paint the bucket',
+            isComplete:false
+          },
+          3: {
+            title: 'Another Todo',
+            isComplete:false
+          }
+        }
+      };
+
+      main.createTodo = function (todo, isValid) {
+        if (isValid) {
+          main.loading = true;
+
+          main.todos[4] = todo;
+
+          main.resetForm();
+        };
+      };
+
+      main.updateTodo = function (todoId, todo, isValid) {
+        if (isValid) {
+          main.loading = true;
+          // UPDATE TODO
+          main.cancelEditing();
+        }
+      };
+
+
+      main.deleteTodo = function (todoId) {
+        main.loading = true;
+        // DELETE TODO
+        main.cancelEditing();
+      };
+
+      main.setEditedTodo = function (todoId, todo) {
+        main.editedTodoId = todoId;
+        main.editedTodo = angular.copy(todo);
+        main.isEditing = true;
+      };
+
+      main.isCurrentTodo = function (todoId) {
+        return main.editedTodo !== null && main.editedTodoId === todoId;
+      };
+
+      main.cancelEditing = function () {
+        main.loading = false;
+        main.editedTodoId = null;
+        main.editedTodo = null;
+        main.isEditing = false;
+      }
+
+      main.getTodos();
 }]);
