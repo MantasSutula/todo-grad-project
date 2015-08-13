7f1c1dd91e0e0a4cd70563611c612767f81ef201
Mantas Sutula <msutula@scottlogic.co.uk>
2015-08-13 10:57:00 +0100

    Created todo template & controller. Working on the index

---
+++ public/app/todoList.js
@@ -0,0 +1,27 @@
+"use strict";
+
+angular.module("todoList", [
+    "ui.router",
+    'ngAnimate'
+])
+    .constant("ENDPOINT_URI", "http://localhost:8080")
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/todos");
+
+        $stateProvider
+            .("todos", {
+            url:"/todos",
+            templateUrl: "app/todos/todos.tmpl.html",
+            controller: "todosCtrl",
+            controllerAs: "todos"
+        })
+    })
+    .run(function ($rootScope, $state) {
+        $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+            event.preventDefault();
+            if (error === 'AUTH_REQUIRED') {
+                $state.go('login');
+            }
+        });
+    })
+;
---
+++ public/app/todos/todos-controller.js
@@ -0,0 +1,6 @@
+angular.module("todoList")
+    .controller("todosCtrl", function() {
+        var todos = this;
+
+        todos.title = "TODO!";
+    });
